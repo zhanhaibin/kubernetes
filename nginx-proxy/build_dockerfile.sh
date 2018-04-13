f00a99d54fc2f0aed0948cb9f4e7d4c184d5ca27
zhanhaibin <haibin.zhan@gmail.com>
2018-04-13 10:45:34 +0800

    update

--- nginx-proxy/build_dockerfile.sh
+++ nginx-proxy/build_dockerfile.sh
@@ -8,14 +8,15 @@ echo '    1. 调用dockerfile创建镜像。                                    
 echo '    2. 参数1，构建的镜像标签，默认为时间戳。                                '
 echo '****************************************************************************'
 # 定义变量
-NAME=colorcoding/nginx
+NAME=colorcoding/nginx 
+RegistoryUrl=docker.avacloud.com.cn
 TAG=$1
 if [ "${TAG}" == "" ]; then TAG=ibas-alpine; fi;
 NAME_TAG=${NAME}:${TAG}
 
 echo 开始构建镜像${NAME_TAG}
 # 调用docker build
-docker build --force-rm --no-cache -f ./dockerfile -t ${NAME_TAG} ./
+docker build --force-rm --no-cache -f ./dockerfile -t ${RegistoryUrl}/${NAME_TAG} ./
 
 if [ "$?" == "0" ]; then
   echo 镜像${NAME_TAG}构建完成
