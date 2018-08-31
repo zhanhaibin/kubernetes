824fcc5eac76bdf86c13400ac646d65adf6f09f5
zhanhaibin <haibin.zhan@gmail.com>
2018-08-31 17:55:24 +0800

    update grant

--- rds-mysql/create_db_user.sh
+++ rds-mysql/create_db_user.sh
@@ -3,9 +3,10 @@
 MYSQL=`which mysql`
 EXPECTED_ARGS=5
 # 创建用户和密码
-# $5 密码
 # $4 用户
-SQL="CREATE USER '$4'@'%' IDENTIFIED BY '$5';"
+# $5 密码
+# S6 账套
+SQL="GRANT  all privileges ON \`$6_%\`.* TO '$4'@'%' IDENTIFIED BY '$5';"
 
 # 登陆mysql
 # $1 登录mysql的地址 
@@ -13,11 +14,15 @@ SQL="CREATE USER '$4'@'%' IDENTIFIED BY '$5';"
 # $3 登录mysql服务器的密码，自行替换
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage Args: $0 hostname dbuser dbpass createuser,createpass"
+  echo "Usage Args: $0 hostname dbuser dbpass createuser,createpass,account"
   exit $E_BADARGS
 fi
 
 echo $MYSQL --host=$1 -P 3306 --user=$2 --password=$3 -e "$SQL"
 result="$($MYSQL --host=$1 -P 3306 --user=$2 --password=$3 -e "$SQL")"
 echo -e "$result"
+
+ 
+
+
 
