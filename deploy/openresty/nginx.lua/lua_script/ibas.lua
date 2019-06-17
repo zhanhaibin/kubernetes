6e80cce4cbe38577316be2625cb359049d4d7ceb
zhanhaibin <haibin.zhan@gmail.com>
2019-06-17 10:45:41 +0800

    update

--- deploy/openresty/nginx.lua/lua_script/ibas.lua
+++ deploy/openresty/nginx.lua/lua_script/ibas.lua
@@ -2,16 +2,17 @@ local _M = {}
 
 local http = require "resty.http"
 local cjson = require "cjson"
-function _M.get_platform_runk8s(token,code,port,namespace)
+local request_console_url = "http://192.168.2.10:30090"
+
+function _M.get_platform_runk8s(code,port,namespace)
   local httpc = http.new()
   -- httpc:set_timeout(100)
   local params = {}
-  params['token'] = token
   params['customerCode'] = code
   params['port'] = port
   params['serverCode'] = 'SERVER'
   params['namespace'] = namespace
-  local res,err = httpc:request_uri("https://console.avacloud.com.cn/customermanagement/services/rest/data/runK8sForOpen?"..ngx.encode_args(params),{
+  local res,err = httpc:request_uri(request_console_url.."/customermanagement/services/rest/data/runK8sForOpen?"..ngx.encode_args(params),{
     method = "GET",
     headers = {
       ["Accept"] = "*",
@@ -26,13 +27,12 @@ function _M.get_platform_runk8s(token,code,port,namespace)
   return res
 end
 
-function _M.get_platform_delK8s(token,code)
+function _M.get_platform_delK8s(code)
   local httpc = http.new()
   local params = {}
-  params['token'] = token
   params['serverCode'] = 'SERVER'
   params['accountCode'] = code
-  local res,err = httpc:request_uri("http://192.168.2.10:30090/operationmaintenance/services/rest/data/delK8sForOpen?"..ngx.encode_args(params),{
+  local res,err = httpc:request_uri(request_console_url.."/operationmaintenance/services/rest/data/delK8sForOpen?"..ngx.encode_args(params),{
     method = "GET",
     headers = {
       ["Accept"] = "*",
@@ -68,40 +68,12 @@ function _M.get_request_uri(uri)
   return res.status
 end
 
-function _M.get_platform_token(user,password)
-  local httpc = http.new()
-  httpc:set_timeout(120)
-  local params = {}
-  params['user'] = user
-  params['password'] = password
-  local res,err = httpc:request_uri("https://console.avacloud.com.cn/initialfantasy/services/rest/data/userConnect?"..ngx.encode_args(params),{
-   method = "POST",
-   headers = {
-      ["Accept"] = "application/json",
-      ["Accept-Encoding"] = "UTF-8",
-   },
-   ssl_verify = false
-  })
-
-  if not res then  return nil  end
-  local token = ''
-  local response = cjson.decode(res.body)
-  for i, w in ipairs(response.ResultObjects) do
-    token = w.Token
-  end
-  httpc:close()
-
-
-  return token
-end
-
-function _M.get_platform_customercode(token,enccode)
+function _M.get_platform_customercode(enccode)
   local httpc = http.new()
   local params = {}
-  params['token'] = token
   params['customerEncCode'] = enccode
   httpc:set_timeout(120)
-  local res,err = httpc:request_uri("https://console.avacloud.com.cn/customermanagement/services/rest/data/searchCustomerForOpen?"..ngx.encode_args(params),{
+  local res,err = httpc:request_uri(request_console_url.."/customermanagement/services/rest/data/searchCustomerForOpen?"..ngx.encode_args(params),{
    method = "GET",
    headers = {
       ["Accept"] = "application/json",
